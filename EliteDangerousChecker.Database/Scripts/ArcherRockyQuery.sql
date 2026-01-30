28b43f2704d142d130623c184de0c2064457af5e
Geerten Vink <maedaros@yahoo.com>
2026-01-30 12:05:31 +0100

    Save query progress for the rocky rings

---
+++ EliteDangerousChecker.Database/Scripts/ArcherRockyQuery.sql
@@ -0,0 +1,28 @@
+﻿with powerSystems as 
+(select 
+	ss.Id SolarSystemId
+from 
+	SolarSystem ss
+	join Power controllingPower on controllingPower.Id = ss.ControllingPowerId
+	join SolarSystemPower ssp on ssp.SolarSystemId = ss.Id
+	join Power anyPower on anyPower.Id = ssp.PowerId
+where
+	controllingPower.Name <> 'Jerome Archer'
+	and anyPower.Name = 'Jerome Archer'),
+withRockyRings as
+(select
+	distinct ps.SolarSystemId
+from
+	Body b
+	join powerSystems ps on ps.SolarSystemId = b.SolarSystemId
+	join Ring r on r.BodyId = b.Id
+	join RingType rt on rt.Id = r.RingTypeId
+where
+	rt.Name = 'Rocky')
+select
+	sp.SolarSystemId, sp.Sequence, spw.Name, sp.SectorPrefixNumber, sp.StartWithDash, sp.StartWithJ
+from
+	SectorPrefix sp
+	join withRockyRings wrr on wrr.SolarSystemId = sp.SolarSystemId
+	left join SectorPrefixWord spw on spw.Id = sp.SectorPrefixWordId
+order by sp.SolarSystemId, sp.Sequence
