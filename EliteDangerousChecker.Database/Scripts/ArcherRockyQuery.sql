ca0736c508ad9334e21e029a0788821d9297de50
Geerten Vink <maedaros@yahoo.com>
2026-02-01 11:00:49 +0100

    Updated query

--- EliteDangerousChecker.Database/Scripts/ArcherRockyQuery.sql
+++ EliteDangerousChecker.Database/Scripts/ArcherRockyQuery.sql
@@ -6,8 +6,9 @@
         exists
         (
             select 1
-            from SolarSystemPower ssp
-            join Power p on p.Id = ssp.PowerId
+            from 
+				SolarSystemPower ssp
+				join Power p on p.Id = ssp.PowerId
             where
                 ssp.SolarSystemId = ss.Id
                 and p.Name = 'Jerome Archer'
@@ -15,7 +16,8 @@
         and exists
         (
             select 1
-            from Power p
+            from 
+				Power p
             where
                 p.Id = ss.ControllingPowerId
                 and p.Name <> 'Jerome Archer'
@@ -23,9 +25,10 @@
         and exists
         (
             select 1
-            from Body b
-            join Ring r on r.BodyId = b.Id
-            join RingType rt on rt.Id = r.RingTypeId
+            from 
+				Body b
+				join Ring r on r.BodyId = b.Id
+				join RingType rt on rt.Id = r.RingTypeId
             where
                 b.SolarSystemId = ss.Id
                 and rt.Name = 'Rocky'
@@ -36,9 +39,13 @@ EligibleStations as
     select
         s.Id as StationId,
         s.SolarSystemId
-    from Station s
-    join EligibleSolarSystems ess
-        on ess.SolarSystemId = s.SolarSystemId
+    from 
+		Station s
+		join EligibleSolarSystems ess on ess.SolarSystemId = s.SolarSystemId
+    where
+		(s.MediumPads > 0 or s.LargePads > 0)
+		and s.PrimaryEconomyId <> 2
+
 ),
 StationCommodityPrices as
 (
@@ -46,12 +53,12 @@ StationCommodityPrices as
         es.StationId,
         es.SolarSystemId,
         rrc.CommodityId,
-        coalesce(sc.SellPrice, rrc.AverageSellPrice) as EffectiveSellPrice
-    from EligibleStations es
-    cross join RockyRingCommodities rrc
-    left join StationCommodities sc
-        on sc.StationId = es.StationId
-       and sc.CommodityId = rrc.CommodityId
+        coalesce(sc.SellPrice, rrc.AverageSellPrice) as EffectiveSellPrice,
+		sc.Demand
+    from 
+		EligibleStations es
+		cross join RockyRingCommodities rrc
+		left join StationCommodities sc on sc.StationId = es.StationId and sc.CommodityId = rrc.CommodityId
 ),
 StationAverages as
 (
@@ -65,9 +72,15 @@ StationAverages as
         max(case when scp.CommodityId = 41 then scp.EffectiveSellPrice end) as Benitoite,
         max(case when scp.CommodityId = 212 then scp.EffectiveSellPrice end) as Monazite,
         max(case when scp.CommodityId = 217 then scp.EffectiveSellPrice end) as Musgravite,
-        max(case when scp.CommodityId = 287 then scp.EffectiveSellPrice end) as Serendibite
+        max(case when scp.CommodityId = 287 then scp.EffectiveSellPrice end) as Serendibite,
 
-    from StationCommodityPrices scp
+		max(case when scp.CommodityId = 11 then scp.Demand end) as AlexandriteDemand,
+		max(case when scp.CommodityId = 41 then scp.Demand end) as BenitoiteDemand,
+		max(case when scp.CommodityId = 212 then scp.Demand end) as MonaziteDemand,
+		max(case when scp.CommodityId = 217 then scp.Demand end) as MusgraviteDemand,
+		max(case when scp.CommodityId = 287 then scp.Demand end) as SerendibiteDemand
+    from 
+		StationCommodityPrices scp
     group by
         scp.StationId,
         scp.SolarSystemId
@@ -81,20 +94,50 @@ RankedStations as
             partition by SolarSystemId
             order by AverageSellPrice desc
         ) as rn
-    from StationAverages
+    from 
+		StationAverages
 )
 select top (50)
     ssn.Name as SolarSystemName,
+	st.Id as StationId,
     st.Name  as StationName,
+	DATEADD(SECOND, MarketUpdateTime, '1970-01-01') UpdateTime,
     rs.AverageSellPrice,
     rs.Alexandrite,
+	rs.AlexandriteDemand,
     rs.Benitoite,
+	rs.BenitoiteDemand,
     rs.Monazite,
+	rs.MonaziteDemand,
     rs.Musgravite,
-    rs.Serendibite
-from RankedStations rs
-join Station st
-    on st.Id = rs.StationId
-cross apply dbo.GetSectorPrefixName(rs.SolarSystemId) ssn
-where rs.rn = 1
-order by rs.AverageSellPrice desc;
+	rs.MusgraviteDemand,
+    rs.Serendibite,
+	rs.SerendibiteDemand,
+    coalesce(
+        (select se.Proportion 
+         from StationEconomies se 
+         join Economy e on e.Id = se.EconomyId 
+         where se.StationId = st.Id and e.Name = 'Refinery'), 
+        0.0
+    ) as RefineryProportion,
+	e.Name PrimaryEconomy,
+	cr.Result,
+	cr.Timestamp,
+	b.Name
+
+from 
+	RankedStations rs
+	join Station st on st.Id = rs.StationId
+	cross apply dbo.GetSectorPrefixName(rs.SolarSystemId) ssn
+	left join Economy e on e.Id = st.PrimaryEconomyId
+	left join CheckResult cr on cr.StationId = st.Id
+	left join Body b on b.Id = st.BodyId
+where 
+	AlexandriteDemand > 20
+	and BenitoiteDemand > 20
+	and MonaziteDemand > 20
+	and MusgraviteDemand > 20
+	and SerendibiteDemand > 20
+order by 
+	rs.AverageSellPrice desc, 
+	UpdateTime desc;
